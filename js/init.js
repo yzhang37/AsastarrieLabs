f567cef17f5480758a0123e7df24d7bdeafc2418
X230大青椒 <Unicorn_X230@outlook.com>
2017-10-03 14:42:38 +0800

    Alpha 0.0.2: continue add features of uploader details pages.

--- js/init.js
+++ js/init.js
@@ -1,6 +1,15 @@
 ---
 
 ---
+
+(function ($) {
+  $.getUrlParam = function (name) {
+      var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+      var r = window.location.search.substr(1).match(reg);
+      if (r != null) return decodeURI(r[2]); return null;
+  }
+})(jQuery);
+
 (function($){
   $(function(){
     //define functions
@@ -26,6 +35,8 @@
       });
     ShowBottom();
 
+
+
     //events
     $('.av-panel').each(function() {
       $av_cnt = $(this).find(".avcard").length;
@@ -36,5 +47,12 @@
     $(window).resize(function() {
       ShowBottom('medium');
     });
+
+        // if there exists a platform link, show it.
+    var platformParam = $.getUrlParam("platform");
+    if (platformParam != null ) {
+      var plat_btn = $('.dropdown-button[platform="' + platformParam + '"]');
+      plat_btn.dropdown('open');
+    }
   }); // end of document ready  
 })(jQuery); // end of jQuery name space
--- page04.md
+++ page04.md
@@ -10,3 +10,9 @@ names:
 permalink: /about/
 unfinished: true
 ---
+{% assign str="red lighten-3" %}
+
+
+{% assign array_class = str | split: ' ' %}
+{% for array_class_ele in array_class %}{% assign num = array_class_ele | split: '-' | last | size %}{% if num == 1 %}{% assign array_class_ele_new = array_class_ele | prepend: "text-" | split: ' ' %}{% else %}{% assign array_class_ele_new = array_class_ele | append: "-text"  | split: ' ' %}{% endif %}{% assign array_class_every = array_class_every | concat: array_class_ele_new %}{% endfor %}
+{% assign newstr = array_class_every | join: ' ' %}
